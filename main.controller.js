7460ce1e57b04a363344639d9164fc3a79c870aa
Diana Sacuiu <pixelized_peanut@icloud.com>
2015-10-08 09:38:35 +0100

    updated the feed to fit with chosen artist

--- main.controller.js
+++ main.controller.js
@@ -1,8 +1,15 @@
 angular
   .module('placebo', [ ])
- 
+  .config(function($locationProvider) { $locationProvider.html5Mode(true); })
   .controller('MainController', function ($scope, $http) {
 
-      $http.get("https://api.spotify.com/v1/artists/0MebO9MFNvedjUBsPGo0YN/albums?market=GB").success(function (response) {$scope.albums = response.items;});
+      $http.get("https://api.spotify.com/v1/artists/6RZUqkomCmb8zCRqc9eznB/albums?market=GB&album_type=album").success(function (response) {$scope.albums = response.items;});
+    
+    //if id in url
+        var url = window.location.search;
+        if(url) var id = url.match(/(\?||&)id=(\w+)&?/)[2];
+        //get id from url
+        if(id) $http.get("https://api.spotify.com/v1/albums/"+id).success(function (response) {$scope.album = response;});
+       
             
   });
